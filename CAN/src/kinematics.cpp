9725a8cec782d57b96a5347454719c44ddb4da4a
Aria Ruan <zhianruan2025@u.northwestern.edu>
2025-02-17 12:07:46 -0800

    Working kinematics

--- CAN/src/kinematics.cpp
+++ CAN/src/kinematics.cpp
@@ -5,19 +5,35 @@
 namespace NP1_Kin
 {
     // joint toruqe to motor torque
-    float torque_j2m()
+    float* torque_j2m(float torque1, float torque2)
     {
-        return;
+        static float torque_motor[3];
+
+        torque_motor[0] = ;
+        
+
+        return torque_motor;
+    }
+
+    // motor angle to joint angle
+    float* angle_m2j(float ang0, float ang1, float ang2)
+    {
+        static float angle_joint[2];
+
+        angle_joint[0] = jacobian[0] * ang0 + jacobian[1] * ang1 + jacobian[2] * ang2; // dip
+        angle_joint[1] = jacobian[3] * ang0 + jacobian[4] * ang1 + jacobian[5] * ang2; // pip
+
+        return angle_joint;
     }
 
     // radian to degree
-    double radToDeg(double ang)
+    float radToDeg(float ang)
     {
         return ang * PI / 180.;
     }
 
     // degree to radian
-    double DegToRad(double ang)
+    float DegToRad(float ang)
     {
         return ang / PI * 180.;
     }
--- CAN/src/kinematics.hpp
+++ CAN/src/kinematics.hpp
@@ -4,27 +4,21 @@
 namespace NP1_Kin
 {
     constexpr double PI = 3.1415;
-
-    // A: structure Jacobian 
-    // [[-R, -R, R],
-    // [-R, -R, 0]]
-    constexpr size_t ROWS = 2;
-    constexpr size_t COLS = 3;
-    constexpr double R = 1.0;  // Replace with your R value
+    constexpr double R = 9.0;  // Replace with your R value
     
-    constexpr double jacobian[ROWS][COLS] = {
-        {-R, -R, R},
-        {-R, -R, 0}
-    };
+    constexpr float jacobian[6] = {-0.333f, -0.333f, 0.333f, -0.5f, 0.5f, 0.0f};
 
     // joint toruqe to motor torque
-    float torque_j2m();
+    float* torque_j2m(float torque1, float torque2);
+
+    // motor angle to joint angle
+    float* angle_m2j(float ang1, float ang2, float ang3);
 
     // radian to degree
-    double radToDeg();
+    float radToDeg(float ang);
 
     // degree to radian
-    double DegToRad();
+    float DegToRad(float ang);
 }
 
 

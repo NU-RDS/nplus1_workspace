e6982caddb9259cd5cdf6d13b1a14582941f9993
Aria Ruan <zhianruan2025@u.northwestern.edu>
2025-02-18 15:25:20 -0800

    Implemented kinematics with force offsets

--- CAN/src/kinematics.cpp
+++ CAN/src/kinematics.cpp
@@ -4,15 +4,92 @@
 
 namespace NP1_Kin
 {
-    // joint toruqe to motor torque
-    float* torque_j2m(float torque1, float torque2)
+
+    // helper: torque to tendon force
+    float* f_tendon(float tor0, float tor1)
     {
-        static float torque_motor[3];
+        static float tendon[3];
+        tendon[0] = A_dagger[0] * tor0 + A_dagger[1] * tor1; // tendon one motor 0
+        tendon[1] = A_dagger[2] * tor0 + A_dagger[3] * tor1;// tendon one motor 1
+        tendon[2] = A_dagger[4] * tor0 + A_dagger[5] * tor1; // tendon one motor 2
 
-        torque_motor[0] = ;
-        
+        return tendon;
+    }
 
-        return torque_motor;
+    // helper: force offset
+    void f_offset(float* tendon)
+    {
+        float alpha = 0; // records the maximum difference of the negative term(s)
+        float temp = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i < 2)
+            {
+                temp = 0. - tendon[i];
+            }
+            else
+            {
+                temp = (0. - tendon[i])/2;
+            }
+            if (temp > alpha)
+            {
+                alpha = temp;
+            }
+        }
+
+        // after we calculated alpha
+        for (int i = 0; i < 3; i++)
+        {
+            if (i < 2)
+            {
+                tendon[i] = tendon[i] + alpha;
+            }
+            else
+            {
+                tendon[i] = tendon[i] + 2 * alpha;
+            }
+        }
+
+    }
+
+
+    // joint toruqe to motor torque
+    float* torque_j2m(float torque0, float torque1)
+    {
+        static float motor_torques[3] = {0};
+        float* force_tendon = f_tendon(torque0, torque1);
+
+        // check if need offset
+        bool positive_force = true;
+        for (int i = 0; i < 3; i++)
+        {
+            positive_force = positive_force && (force_tendon[i] > 0.);
+        }
+
+        // any tenson force not positive
+        if (!positive_force)
+        {
+            f_offset(force_tendon);
+        }
+
+        // now all force on tendon should be positive
+        // calculate for motor torque 
+        float* motor_torques[3];
+        for (int i = 0; i < 3; i++)
+        {
+           float motor_torque = force_tendon[i] * R;
+           if (motor_torque < STALL_TORQUE)
+           {
+            motor_torque = STALL_TORQUE;
+           }
+           if (motor_torque > MAX_TORQUE)
+           {
+            motor_torque = MAX_TORQUE;
+           }
+           motor_torques[i] = motor_torque;
+        }
+
+        return motor_torques;
     }
 
     // motor angle to joint angle
@@ -20,8 +97,8 @@ namespace NP1_Kin
     {
         static float angle_joint[2];
 
-        angle_joint[0] = jacobian[0] * ang0 + jacobian[1] * ang1 + jacobian[2] * ang2; // dip
-        angle_joint[1] = jacobian[3] * ang0 + jacobian[4] * ang1 + jacobian[5] * ang2; // pip
+        angle_joint[0] = jacobian_ang[0] * ang0 + jacobian_ang[1] * ang1 + jacobian_ang[2] * ang2; // dip
+        angle_joint[1] = jacobian_ang[3] * ang0 + jacobian_ang[4] * ang1 + jacobian_ang[5] * ang2; // pip
 
         return angle_joint;
     }
@@ -29,13 +106,13 @@ namespace NP1_Kin
     // radian to degree
     float radToDeg(float ang)
     {
-        return ang * PI / 180.;
+        return ang / PI * 180.;
     }
 
     // degree to radian
     float DegToRad(float ang)
     {
-        return ang / PI * 180.;
+        return ang * PI / 180.;
     }
 }
 
--- CAN/src/kinematics.hpp
+++ CAN/src/kinematics.hpp
@@ -5,8 +5,17 @@ namespace NP1_Kin
 {
     constexpr double PI = 3.1415;
     constexpr double R = 9.0;  // Replace with your R value
-    
-    constexpr float jacobian[6] = {-0.333f, -0.333f, 0.333f, -0.5f, 0.5f, 0.0f};
+    constexpr float STALL_TORQUE = .0036;
+    constexpr float MAX_TORQUE = .5;
+    constexpr float jacobian_ang[6] = {-0.333f, -0.333f, 0.333f, -0.5f, 0.5f, 0.0f}; // 2x3
+
+    constexpr float jacobian_tor[6] = {0};
+    constexpr float A_dagger[6] = {-1/3/R, -1/3/R, 1/3/R, -1/2/R, 1/2/R, 0.f}; // 2x3
+
+    // helper: torque to tendon force
+    float* f_tendon(float tor0, float tor1);
+    // helper: force offset
+    void f_offset(float ang1, float ang2, float ang3);
 
     // joint toruqe to motor torque
     float* torque_j2m(float torque1, float torque2);

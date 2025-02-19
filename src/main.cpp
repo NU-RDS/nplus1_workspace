8b896ab85931b58debba0181bcef11d522fc5bd0
Aria Ruan <zhianruan2025@u.northwestern.edu>
2025-02-19 14:10:36 -0800

    Moved encoder feedback to serial command

--- src/main.cpp
+++ src/main.cpp
@@ -108,9 +108,18 @@ void processSerialCommand() {
                     odrives[driveNum].is_running = true;
                     odrives[driveNum].drive.setTorque(torque);
 
+                    Get_Encoder_Estimates_msg_t feedback = odrives[driveNum].user_data.last_feedback;
+
                     Serial.print("Starting ODrive ");
                     Serial.print(driveNum);
-                    Serial.println(clockwise ? " CW" : " CCW");
+                    Serial.print(clockwise ? " CW " : " CCW" );
+                    
+                    Serial.print("ODrive ");
+                    Serial.print(driveNum);
+                    Serial.print(" - Position: ");
+                    Serial.print(feedback.Pos_Estimate);
+                    Serial.print(", Velocity: ");
+                    Serial.println(feedback.Vel_Estimate);
                 }
             }
         }
@@ -163,13 +172,13 @@ void loop() {
             unsigned long current_time = millis();
             
             if (current_time - last_feedback_time >= FEEDBACK_DELAY) {
-                Get_Encoder_Estimates_msg_t feedback = odrives[i].user_data.last_feedback;
-                Serial.print("ODrive ");
-                Serial.print(i);
-                Serial.print(" - Position: ");
-                Serial.print(feedback.Pos_Estimate);
-                Serial.print(", Velocity: ");
-                Serial.println(feedback.Vel_Estimate);
+                // Get_Encoder_Estimates_msg_t feedback = odrives[i].user_data.last_feedback;
+                // Serial.print("ODrive ");
+                // Serial.print(i);
+                // Serial.print(" - Position: ");
+                // Serial.print(feedback.Pos_Estimate);
+                // Serial.print(", Velocity: ");
+                // Serial.println(feedback.Vel_Estimate);
                 last_feedback_time = current_time;
             }
         }

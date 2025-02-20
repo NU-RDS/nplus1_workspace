b5911c5f13b12df39d094a80354c24ec96999828
Aria Ruan <zhianruan2025@u.northwestern.edu>
2025-02-19 17:37:15 -0800

    Added kinamatics.hpp

--- include/kinematics.hpp
+++ include/kinematics.hpp
@@ -4,25 +4,30 @@
 namespace NP1_Kin
 {
     constexpr double PI = 3.1415;
+    constexpr double R = 9.0;  // Replace with your R value
+    constexpr float STALL_TORQUE = .0036;
+    constexpr float MAX_TORQUE = .5;
+    constexpr float jacobian_ang[6] = {-0.333f, -0.333f, 0.333f, -0.5f, 0.5f, 0.0f}; // 2x3
 
-    // A: structure Jacobian 
-    // [[-R, -R, R],
-    // [-R, -R, 0]]
-    constexpr size_t ROWS = 2;
-    constexpr size_t COLS = 3;
-    constexpr double R = 1.0;  // Replace with your R value
-    
-    constexpr double jacobian[ROWS][COLS] = {
-        {-R, -R, R},
-        {-R, -R, 0}
-    };
+    constexpr float jacobian_tor[6] = {0};
+    constexpr float A_dagger[6] = {-1/3/R, -1/3/R, 1/3/R, -1/2/R, 1/2/R, 0.f}; // 2x3
+
+    // helper: torque to tendon force
+    float* f_tendon(float tor0, float tor1);
+    // helper: force offset
+    void f_offset(float ang1, float ang2, float ang3);
 
     // joint toruqe to motor torque
-    float torque_j2m();
+    float* torque_j2m(float torque1, float torque2);
+
+    // motor angle to joint angle
+    float* angle_m2j(float ang1, float ang2, float ang3);
 
     // radian to degree
-    double radToDeg();
+    float radToDeg(float ang);
 
     // degree to radian
-    double DegToRad();
+    float DegToRad(float ang);
 }
+
+

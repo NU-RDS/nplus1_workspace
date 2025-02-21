4a5f4a3c102d716e0b2260f6dbbc8a841da1dfbe
Aria Ruan <zhianruan2025@u.northwestern.edu>
2025-02-20 19:21:03 -0800

    Added kinematics

--- include/kinematics.hpp
+++ include/kinematics.hpp
@@ -1,28 +1,34 @@
 #pragma once
 #include <cmath>
+#include <iostream>
+
 
 namespace NP1_Kin
 {
-    constexpr double PI = 3.1415;
+    constexpr float PI = 3.1415;
+    constexpr float R = .009;  // Replace with your R value
+    constexpr float STALL_TORQUE = .0036;
+    constexpr float MAX_TORQUE = .04;
+    constexpr float jacobian_ang[6] = {-0.33333f, -0.33333f, 0.33333f, -0.5f, 0.5f, 0.0f}; // 2x3
 
-    // A: structure Jacobian 
-    // [[-R, -R, R],
-    // [-R, -R, 0]]
-    constexpr size_t ROWS = 2;
-    constexpr size_t COLS = 3;
-    constexpr double R = 1.0;  // Replace with your R value
-    
-    constexpr double jacobian[ROWS][COLS] = {
-        {-R, -R, R},
-        {-R, -R, 0}
-    };
+    // constexpr float jacobian_tor[6] = {0};
+    constexpr float A_dagger[6] = {-37.037037, -55.555555, -37.037037, 55.555555, 37.037037, 0.};
+
+    // helper: torque to tendon force
+    float* f_tendon(float tor0, float tor1);
+    // helper: force offset
+    void f_offset(float* tendon);
 
     // joint toruqe to motor torque
-    float torque_j2m();
+    float* torque_j2m(float torque1, float torque2);
+
+    // motor angle to joint angle
+    float* angle_m2j(float ang1, float ang2, float ang3);
 
     // radian to degree
-    double radToDeg();
+    float radToDeg(float ang);
 
     // degree to radian
-    double DegToRad();
+    float DegToRad(float ang);
 }
+
--- src/control/kinematics.cpp
+++ src/control/kinematics.cpp
@@ -1,24 +1,116 @@
-// #pragma once
-// #include <cmath>
-// #include "kinematics.hpp"
+#include <cmath>
+#include "kinematics.hpp"
 
-// namespace NP1_Kin
-// {
-//     // joint toruqe to motor torque
-//     float torque_j2m()
-//     {
-//         return;
-//     }
+using namespace std;
 
-//     // radian to degree
-//     double radToDeg(double ang)
-//     {
-//         return ang * PI / 180.;
-//     }
+namespace NP1_Kin
+{
 
-//     // degree to radian
-//     double DegToRad(double ang)
-//     {
-//         return ang / PI * 180.;
-//     }
-// }
+    // helper: torque to tendon force
+    float* f_tendon(float tor0, float tor1)
+    {
+        static float tendon[3];
+        tendon[0] = A_dagger[0] * tor0 + A_dagger[1] * tor1; // tendon one motor 0
+        tendon[1] = A_dagger[2] * tor0 + A_dagger[3] * tor1;// tendon one motor 1
+        tendon[2] = A_dagger[4] * tor0 + A_dagger[5] * tor1; // tendon one motor 2
+        return tendon;
+    }
+
+    // helper: force offset
+    void f_offset(float* tendon)
+    {
+        float alpha = 0; // records the maximum difference of the negative term(s)
+        float temp = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i < 2)
+            {
+                temp = 0. - tendon[i];
+            }
+            else
+            {
+                temp = (0. - tendon[i])/2;
+            }
+            if (temp > alpha)
+            {
+                alpha = temp;
+            }
+        }
+        // after we calculated alpha
+        for (int i = 0; i < 3; i++)
+        {
+            if (i < 2)
+            {
+                tendon[i] = tendon[i] + alpha;
+            }
+            else
+            {
+                tendon[i] = tendon[i] + 2 * alpha;
+            }
+        }
+
+    }
+
+
+    // joint toruqe to motor torque
+    float* torque_j2m(float torque0, float torque1)
+    {
+        static float motor_torques[3] = {0};  // Keep only this declaration
+        float* force_tendon = f_tendon(torque0, torque1);
+    
+        // check if need offset
+        bool positive_force = true;
+        for (int i = 0; i < 3; i++)
+        {
+            positive_force = positive_force && (force_tendon[i] > 0.);
+        }
+    
+        // any tenson force not positive
+        if (!positive_force)
+        {
+            f_offset(force_tendon);
+        }
+    
+        // now all force on tendon should be positive
+        // calculate for motor torque 
+        for (int i = 0; i < 3; i++)
+        {
+           float motor_torque = force_tendon[i] * R;
+           if (motor_torque < STALL_TORQUE)
+           {
+               motor_torque = STALL_TORQUE;
+           }
+           if (motor_torque > MAX_TORQUE)
+           {
+               motor_torque = MAX_TORQUE;
+           }
+           motor_torques[i] = motor_torque;  // Store directly in the static array
+        }
+    
+        return motor_torques;
+    }
+
+    // motor angle to joint angle
+    float* angle_m2j(float ang0, float ang1, float ang2)
+    {
+        static float angle_joint[2];
+
+        angle_joint[0] = jacobian_ang[0] * ang0 + jacobian_ang[1] * ang1 + jacobian_ang[2] * ang2; // dip
+        angle_joint[1] = jacobian_ang[3] * ang0 + jacobian_ang[4] * ang1 + jacobian_ang[5] * ang2; // pip
+
+        return angle_joint;
+    }
+
+    // radian to degree
+    float radToDeg(float ang)
+    {
+        return ang / PI * 180.;
+    }
+
+    // degree to radian
+    float DegToRad(float ang)
+    {
+        return ang * PI / 180.;
+    }
+}
+
